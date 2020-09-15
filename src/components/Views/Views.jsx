69ebf5e2c2c1747cabdab5a7a7f9d19bebfb1ce9
Maxim Remizov <maxim_remizov@epam.com>
2020-09-16 00:47:41 +0300

    feat: add images for views block

--- src/components/Views/Views.jsx
+++ src/components/Views/Views.jsx
@@ -1,5 +1,36 @@
 import React from "react"
+import Description from "../common/Description/Description"
+import './views.css'
+import view1 from '../../images/view1.png'
+import view2 from '../../images/view2.png'
+import view3 from '../../images/view3.png'
+import view4 from '../../images/view4.png'
+import view5 from '../../images/view5.png'
+import view6 from '../../images/view6.png'
 
-const Views = () => <section className="views"></section>
+
+const Views = function () {
+    return (
+        <section className="views">
+            <Description title='Как всё это выглядит?' />
+            <div className="views_puzzle">
+                <div className="views_puzzle-big50 views_puzzle-horizontal">
+                    <img src={view1} alt="puz" />
+                    <div className="views_puzzle-small50">
+                        <img src={view2} alt="puz" />
+                        <img src={view3} alt="puz" />
+                    </div>
+                </div>
+                <div className="views_puzzle-big50 views_puzzle-vertical">
+                    <img src={view4} alt="puz" />
+                    <div className="views_puzzle-small50">
+                        <img src={view5} alt="puz" />
+                        <img src={view6} alt="puz" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
 
 export default Views
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from "react"
 import Header from "../components/Header/Header"
 import Offer from "../components/Offer/Offer"
 import Counter from "../components/Counter/Counter"
+import Services from "../components/Services/Services"
+import Views from "../components/Views/Views"
 import Button from "../components/common/Button/Button"
 
 import SEO from "../components/seo"
 import "normalize.css"
 import "../fonts/fonts.css"
 import "../components/common/grid.css"
-import Services from "../components/Services/Services"
 
 const IndexPage = () => (
   <>
@@ -21,6 +22,8 @@ const IndexPage = () => (
     <Counter />
 
     <Services />
+
+    <Views />
 
     <Button linkBtn="#" linkText="Больше о нас" linkType="btn-green" />
     <Button linkBtn="#" linkText="Больше о нас" linkType="btn-green-full" />
