086284a26a381839fa9ada97dac9059f6a2ce825
Maxim Remizov <maxim_remizov@epam.com>
2020-09-16 23:09:53 +0300

    fix: change index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import Views from "../components/Views/Views"
 import Elephants from "../components/Elephants/Elephants"
 import Reviews from "../components/Reviews/Reviews"
 import Whois from "../components/Whois/Whois"
-import Button from "../components/common/Button/Button"
 import Success from "../components/Success/Success"
 import Blog from "../components/Blog/Blog"
 
@@ -19,29 +18,19 @@ import "../components/common/grid.css"
 const IndexPage = () => (
   <>
     <SEO title="Emailmatrix" />
-
     <Header />
+    <main>
+      <Offer />
+      <Counter />
+      <Services />
+      <Views />
+      <Elephants />
+      <Reviews />
+      <Whois />
+      <Success />
+      <Blog />
 
-    <Offer />
-
-    <Counter />
-
-    <Services />
-
-    <Views />
-
-    <Elephants />
-
-    <Reviews />
-
-    <Whois />
-
-    <Success />    
-
-    <Blog />
-
-    <Button linkBtn="#" linkText="Больше о нас" linkType="btn-green" />
-    <Button linkBtn="#" linkText="Больше о нас" linkType="btn-green-full" />
+    </main>
   </>
 )
 
