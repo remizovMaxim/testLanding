396a2aca1d20515d9098d73c5cd107a8681fc5d8
Maxim Remizov <maxim_remizov@epam.com>
2020-09-17 13:16:18 +0300

    style: adaptation all blocks to 1024px

--- src/components/Views/Views.jsx
+++ src/components/Views/Views.jsx
@@ -15,17 +15,17 @@ const Views = function () {
             <Description title='Как всё это выглядит?' />
             <div className="views_puzzle">
                 <div className="views_puzzle-big50 views_puzzle-horizontal">
-                    <img src={view1} alt="puz" />
-                    <div className="views_puzzle-small50">
-                        <img src={view2} alt="puz" />
-                        <img src={view3} alt="puz" />
+                    <img src={view1} style={{height: '50%', width: '100%'}} alt="puz" />
+                    <div className="views_puzzle-small50" style={{height: '50%'}}>
+                        <div><img src={view2} alt="puz" /></div>                        
+                        <div><img src={view3} alt="puz" /></div>
                     </div>
                 </div>
                 <div className="views_puzzle-big50 views_puzzle-vertical">
-                    <img src={view4} alt="puz" />
-                    <div className="views_puzzle-small50">
-                        <img src={view5} alt="puz" />
-                        <img src={view6} alt="puz" />
+                    <img src={view4} alt="puz" style={{width: '50%', objectFit:'cover'}}/>
+                    <div className="views_puzzle-small50" style={{width: '50%', height:'100%'}}>
+                        <div style={{height:'50%'}}><img src={view5} alt="puz" /></div>
+                        <div style={{height:'50%'}}><img src={view6} alt="puz" /></div>
                     </div>
                 </div>
             </div>
